a41b4f65803096f14894c4b5b3fa0597a1d760a2
absurezak-shifa <abdushifa60@gmail.com>
2023-04-12 10:26:47 +0300

    3rd commit

--- clients.py
+++ clients.py
@@ -2,24 +2,24 @@ import socket
 
 # Get the client name and integer from the user
 name = input("Enter your name: ")
-num = input("Enter an integer between 1 and 100: ")
+number = input("Enter an integer between 1 and 100: ")
 
 # Open a TCP socket and send the message to the server
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('10.5.232.129', 8000))
-client_socket.send(bytes(name + "," + num, 'utf-8'))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 8500))
+client.send(bytes(name + "," + number, 'utf-8'))
 
 # Wait for the server's reply
-server_reply = client_socket.recv(1024)
-server_name, server_num = server_reply.decode('utf-8').split(",")
+serverReply = client.recv(1024)
+serverName, serverNum = serverReply.decode('utf-8').split(",")
 
 # Compute sum and Print the results 
 print("Client Name:", name)
-print("Server Name:", server_name)
-print("Client Number:", num)
-print("Server Number:", server_num)
-sum = int(num) + int(server_num)
-print("Sum:", sum)
+print("Server Name:", serverName)
+print("Client Number:", number)
+print("Server Number:", serverNum)
+total = int(number) + int(serverNum)
+print("Sum:", total)
 
 # Closing the client socket
-client_socket.close()
+client.close()
--- serverss.py
+++ serverss.py
@@ -2,42 +2,42 @@ import socket
 import random
 
 # Create a TCP socket and start listening for incoming connections
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('10.5.232.129', 8000))
-server_socket.listen(1)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("127.0.0.1", 8500))
+server.listen(1)
 
 # Accept incoming connections from clients
 while True:
     print("Waiting for client to enter what they want...")
-    client_socket, address = server_socket.accept()
+    client, address = server.accept()
     print("Client connected:", address)
 
     # Receive the message from the client
-    client_message = client_socket.recv(1024).decode('utf-8')
-    client_name, client_num = client_message.split(",")
-    print("Client Name:", client_name)
+    client_msg = client.recv(1024).decode('utf-8')
+    clientName, clientNum = client_msg.split(",")
+    print("Client Name:", clientName)
     print("Server Name: Server of John Q. Smith")
 
     # Pick a random number between 1 and 100
-    server_num = str(random.randint(1, 100))
+    serverNum = str(random.randint(1, 100))
 
     # Display the client's number, the server's number, and the sum
-    print("Client Number:", client_num)
-    print("Server Number:", server_num)
-    sum = int(client_num) + int(server_num)
-    print("Sum:", sum)
+    print("Client Number:", clientNum)
+    print("Server Number:", serverNum)
+    total = int(clientNum) + int(serverNum)
+    print("Sum:", total)
 
     # Send the server's name and number back to the client
-    server_message = "Server of John Q. Smith," + server_num
-    client_socket.send(bytes(server_message, 'utf-8'))
+    server_msg = "Server of John Q. Smith," + serverNum
+    client.send(bytes(server_msg, 'utf-8'))
 
     # Check if the client's number is out of range
-    if int(client_num) < 1 or int(client_num) > 100:
+    if int(clientNum) < 1 or int(clientNum) > 100:
         print("Client Number is out of range")
         break
 
     # Close the client socket
-    client_socket.close()
+    client.close()
 
 # Close the server socket
-server_socket.close()
+server.close()
